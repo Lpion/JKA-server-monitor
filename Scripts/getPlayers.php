3f00e7c55d71d6e50a99c2eb28296bfca187791a
lpion <dev@lpion.com>
2021-04-17 21:43:18 +0200

    restructure arrays
    - return more and structured info for FrontEnd
    - moved form index.php

---
+++ Scripts/getPlayers.php
@@ -0,0 +1,52 @@
+<?php
+// Connection
+$host = "64.94.95.202";
+$port = 29070;
+
+// UDP Socket
+$socket = socket_create (AF_INET, SOCK_DGRAM, SOL_UDP);
+
+// jka getstatus request
+$jka_request = str_repeat(chr(0xFF),4)."getstatus\n";
+
+// Send UDP Packet
+socket_sendto($socket, $jka_request, strlen($jka_request), 0x0, $host, $port);  
+
+do
+{
+// 4 bytes for the header + 2000 bytes of data
+socket_recvfrom($socket, $buffer, 2004, 0, $host, $port);
+
+// append the data to the return variable
+$ret .= substr($buffer, 4);
+}
+while(strlen($buffer) == 2000);  // the first non-full packet is the last
+
+// all information is returned as single string, so we make an array
+$retArray = explode("\\",$ret);
+// all player infos are in a single string, so they get seperated into an array
+$playerinfo = array_slice( explode("\n",$retArray[86]), 1, -1);
+// encode to cover special characters in names
+$playerinfo = array_map('utf8_encode', $playerinfo);
+// Create array that will be returned for AJAX call
+$jkaData = array(
+    "mod" => $retArray[82],
+    "map" => $retArray[74],
+    "players" => array()
+);
+
+// optimize player array
+foreach ($playerinfo as $value) {
+    // Seperate player names from ping and id 
+    $player = preg_split('~"[^"]*"(*SKIP)(*F)|\s+~', $value);
+    // Remove quotes surrounding player names
+    $player[2] = trim($player[2], '"');
+    // Remove first index, as its not needed
+    array_splice($player, 0, 1);
+    //add player infos to jkaData
+    array_push($jkaData['players'],$player);
+}
+
+// return Data for Front End
+echo json_encode($jkaData);
+?>
