2e3940f5546c84fe38c727df50acb742281e4bef
lpion <dev@lpion.com>
2021-04-17 03:41:52 +0200

    Rework with php_socket

--- index.php
+++ index.php
@@ -1,42 +1,35 @@
 <?php
 $debug = true;
 
-$server_ip = 'udp://64.94.95.202';
-$server_port = '29070';
+// Connection
+$host = "64.94.95.202";
+$port = 29070;
 
-//Check general connection
-$connect = fsockopen($server_ip, $server_port, $errno, $errstr, 1);
+// UDP Socket
+$socket = socket_create (AF_INET, SOCK_DGRAM, SOL_UDP);
 
-if ( ! $connect ) {
-   if($debug){
-      echo "Error #$errno : $errstr <br>";
-   } 
-   echo 'Server is OFFLINE <br>';     
+// jka getstatus request
+$jka_request = str_repeat(chr(0xFF),4)."getstatus";
+
+// Send UDP Packet
+socket_sendto($socket, $jka_request, strlen($jka_request), 0x0, $host, $port);  
+
+do
+{
+// 4 bytes for the header + 2000 bytes of data
+socket_recvfrom($socket, $buffer, 2004, 0, $host, $port);
+
+// append the data to the return variable
+$ret .= substr($buffer, 4);
 }
-else { 
-   echo 'Server is ONLINE <br>';
-}
+while(strlen($buffer) == 2000);  // the first non-full packet is the last
 
-$send = "\xFF\xFF\xFF\xFFgetstatus\n"; 
+// match everything between "" including them
+$pattern = '/"(.+?)"/';
+preg_match_all($pattern, $ret, $names);
 
-fputs($connect, $send);
-fwrite ($connect, $send);
-$output = fread ($connect, 1);
-if (! empty ($output)) {
-   do {
-     $status_pre = socket_get_status ($connect);
-     $output = $output . fread ($connect, 1);
-     $status_post = socket_get_status ($connect);
-   } while ($status_pre["unread_bytes"] != $status_post["unread_bytes"]);
+// Output all matches as a list
+foreach($names[0] as $player) {   
+  echo "$player\n<br>";
 };
-
-
-// Close the connection:
-fclose($connect);
-
-// // Select the variables from the $output array:
-$output = explode ("\\", $output); 
-
-print_r($output);
-
 ?>
