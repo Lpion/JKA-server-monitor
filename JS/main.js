7dd27af20364cc9b8416c2d7d1ac8a98c420e4e2
lpion <dev@lpion.com>
2021-04-18 18:00:50 +0200

    colorize playernames and small improvments:
    - provide parameters for the php script
    - shorten the output logic

--- JS/main.js
+++ JS/main.js
@@ -3,27 +3,43 @@ function getServerInfo() {
     $.ajax({
       url: "./Scripts/getPlayers.php",
       type: "POST",
+      data: {host: "64.94.95.202", port: "29070"},
+      dataType: "json",
       success: function (jkaData) {
 
         // create playername list
-        for (let i = 0; i < jkaData['players'].length; i++) {
-          
+        for (let i = 0; i < jkaData['players'].length; i++) {    
           const playername = jkaData['players'][i][1];
 
-          // if PING data !== 0 it is a player
-          // otherwise its a bot
-          if(jkaData['players'][i][0] !== '0' ){
-            $('#playerlist').append('<li>' + playername + '</li>');
-          } else {
-            $('#botlist').append('<li>' + playername + '</li>');
-          }
-        }
+          // if PING data == 0 it is a player
+          const listtype = jkaData['players'][i][0] == '0' ? '#botlist' : '#playerlist';
 
+          // Output playerlist
+          $(listtype).append('<li>' + colorize('^7' + playername) + '</li>');
+
+        }
         // output server info
         $('#plugin').append(jkaData['mod']);
         $('#map').append(jkaData['map']);
-      },
-      dataType: "json"
+      }
     });
   }
   getServerInfo()
+
+  function colorize(playername){
+    let colors = ['#000000','#f31415','#0af60a','#fefe00','#0d0df8','#0ef3f4','#fd02fe','#ffffff','#fd7d00', '#8a8e93']
+    let prevCaret = playername.length;
+    let colorizedName = '';
+
+      for (let i = playername.length; i >= 0; i--) {
+        if (playername.charAt(i) == '^' && (playername.charAt(i+1) >= '0' && playername.charAt(i+1) <= '9')) {
+          
+          let colorInt = parseInt(playername.charAt(i+1));
+          let tintedChars = playername.substring(i+2, prevCaret);
+
+          prevCaret = i;
+          colorizedName = tintedChars.fontcolor(colors[colorInt]) + colorizedName;
+        }
+      }
+      return colorizedName
+  }
