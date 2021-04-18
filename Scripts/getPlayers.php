fdd9ecedcdef5d8bf41ec107e580a6b566402252
lpion <dev@lpion.com>
2021-04-18 17:59:02 +0200

    make it accessible for any q3 engine based game
    - host and port can be given as parameter now from eg a form
    - replace hardcoded array index with string searches

--- Scripts/getPlayers.php
+++ Scripts/getPlayers.php
@@ -1,7 +1,7 @@
 <?php
 // Connection
-$host = "64.94.95.202";
-$port = 29070;
+$host = $_POST['host'];
+$port = $_POST['port'];
 
 // UDP Socket
 $socket = socket_create (AF_INET, SOCK_DGRAM, SOL_UDP);
@@ -25,13 +25,13 @@ while(strlen($buffer) == 2000);  // the first non-full packet is the last
 // all information is returned as single string, so we make an array
 $retArray = explode("\\",$ret);
 // all player infos are in a single string, so they get seperated into an array
-$playerinfo = array_slice( explode("\n",$retArray[86]), 1, -1);
+$playerinfo = array_slice( explode("\n",end($retArray)), 1, -1);
 // encode to cover special characters in names
 $playerinfo = array_map('utf8_encode', $playerinfo);
 // Create array that will be returned for AJAX call
 $jkaData = array(
-    "mod" => $retArray[82],
-    "map" => $retArray[74],
+    "mod" => $retArray[array_search('gamename',$retArray) + 1],
+    "map" => $retArray[array_search('mapname',$retArray) + 1],
     "players" => array()
 );
 
