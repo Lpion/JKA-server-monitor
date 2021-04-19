8ab9787f0b8911c7a36900092937df41422281d5
lpion <dev@lpion.com>
2021-04-19 08:04:06 +0200

    seperate blocks into their own functions
    - also grammar: accounting for 0 in online list headers

--- JS/main.js
+++ JS/main.js
@@ -7,17 +7,8 @@ function getServerInfo() {
       dataType: "json",
       success: function (jkaData) {
 
-        // create playername list
-        for (let i = 0; i < jkaData['players'].length; i++) {    
-          const playername = jkaData['players'][i][1];
-
-          // if PING data == 0 it is a player
-          const listtype = jkaData['players'][i][0] == '0' ? '#botlist' : '#playerlist';
-
-          // Output playerlist
-          $(listtype).append('<li>' + colorize('^7' + playername) + '</li>');
-
-        }
+        createOnlineLists(jkaData)
+        
         // output server info
         $('#plugin').append(jkaData['mod']);
         $('#map').append(jkaData['map']);
@@ -25,6 +16,31 @@ function getServerInfo() {
     });
   }
   getServerInfo()
+
+  function createOnlineLists(jkaData){
+    // create playername list
+    for (let i = 0; i < jkaData['players'].length; i++) {    
+      const playername = jkaData['players'][i][1];
+
+      // if PING data == 0 it is a player
+      const listtype = jkaData['players'][i][0] == '0' ? '#botlist' : '#playerlist';
+
+      // Output playerlist
+      $(listtype).append('<li>' + colorize('^7' + playername) + '</li>');
+    }
+
+    createOnlineListCounters()
+
+  }
+
+  function createOnlineListCounters() {
+    // count bots and players based off created ul li items
+    const botcount = $('#botlist li').length 
+    const playercount = $('#playerlist li').length
+    // output counts and text based of count length
+    $("#bot-heading").append(botcount > 2 || botcount == 0 ? botcount + ' Bots' : botcount + ' Bot');
+    $("#player-heading").append(playercount > 1 || playercount == 0 ? playercount + ' Players' : playercount + ' Player');
+  }
 
   function colorize(playername){
     let colors = ['#000000','#f31415','#0af60a','#fefe00','#0d0df8','#0ef3f4','#fd02fe','#ffffff','#fd7d00', '#8a8e93']
